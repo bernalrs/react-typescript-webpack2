9555a79edf379b77ea39578960fc820ca456f649
bernalrs <bernal.rs@gmail.com>
2017-03-18 17:15:15 +0000

    Adding configuration files.

---
+++ webpack.common.config.js
@@ -0,0 +1,35 @@
+const {resolve} = require('path');
+const webpack =require('webpack');
+
+module.exports = {
+    entry: [
+        './index.tsx'
+    ],
+    output:{
+        filename: 'bundle.js',
+        path: resolve(__dirname, 'static'),
+        publicPath: ''
+    },
+    resolve:{
+        extensions: ['js', '.jsx', '.ts', '.tsx', '.css']
+    }
+
+    context: resolve(__dirname, 'src'),
+    devtool: 'inline-source-map',
+    devServer:{
+        hot: true,
+        contentBase: resolve(__dirname, 'static'),
+        publicPath: ''
+    },
+    module: {
+        rules:[{
+                test: /\.(ts|tsx)$/,
+                use: ['awesome-typescript-loader']
+            },{
+                test: /\.(ts|tsx)$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    plugins: []
+}
